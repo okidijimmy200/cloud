dc5bf1533d37e5a5ebad9517a966841286fa4f42
okidijimmy200 <okidijimmie@gmail.com>
2023-05-01 15:30:09 +0300

    userpoolclientID

--- .history/js/config_20230501141447.js
+++ .history/js/config_20230501141447.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_do8VjQmdg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3m9i9aup1lsj19uk126t1nc4ul', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'd1rlkhcfxmfms6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
